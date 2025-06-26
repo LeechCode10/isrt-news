f89605de9433dc69717679917511bf77fad1a450
LeechCode10 <leechcode10@gmail.com>
2025-06-26 09:41:18 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function nigga() {
+    document.getElementById("nword1").innerHTML = "Nigga click the link and read more!!!"
+}
+
+
+function stop() {
+    document.getElementById("nword2").innerHTML = "Stop clicking Read More ffs! My budget is tight for that shit-"
+}
