b83304dde7da87720112cf65b856b8e9bc50e9cd
LeechCode10 <leechcode10@gmail.com>
2025-07-11 19:50:38 +0600

    Add files via upload

--- portals/portal.js
+++ portals/portal.js
@@ -1,21 +1,3 @@
-function nigga() {
-    document.getElementById("nword1").innerHTML = "Nigga click the link and read more!!!"
-}
-
-
-function stop() {
-    document.getElementById("nword2").innerHTML = "Stop clicking Read More ffs! My budget is tight for that shit-"
-}
-
-
-function fuckit() {
-    document.getElementById("nword3").innerHTML = "Fuck it man! I am not putting another Read more button next time."
-}
-
-function rickroll() {
-    window.location.href = "https://youtu.be/xvFZjo5PgG0?si=uoxvYQnfXFLu_h_C"
-}
-
 let isAdmin = false;
 
 // Use a unique key per page based on the URL path
